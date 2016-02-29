897b838a2e5b09a04e150796b9892d2243093723
songteng <songteng@jd.com>
2016-02-29 09:10:34 +0800

    100 7 66

---
+++ leetcode/CMakeFiles/leetcode.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leetcode.dir/delete_node_in_a_linked_list.cpp.o"
+  "leetcode.pdb"
+  "leetcode"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/leetcode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
